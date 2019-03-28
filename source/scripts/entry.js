13456cfefeb3fb342bd0ec5a44ced07e7903476c
loremlipsum <lipsum.webdevelop@gmail.com>
2019-03-28 09:52:03 +0300

    Добавляет bio

--- source/scripts/entry.js
+++ source/scripts/entry.js
@@ -7,3 +7,4 @@ require('../pug/blocks/header/header');
 require('../pug/blocks/field/field');
 require('../pug/blocks/modal/modal');
 // require('../pug/blocks/intro/intro');
+require('../pug/blocks/bio/bio');
