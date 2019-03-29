26f4928b3c08ad7efbdf7edbd1a9d6eb9fb44117
loremlipsum <lipsum.webdevelop@gmail.com>
2019-03-29 08:11:34 +0300

    Добавляет модалку

--- source/pug/blocks/modal/modal.js
+++ source/pug/blocks/modal/modal.js
@@ -1,40 +1,42 @@
 import { $, $$ } from '../../../scripts/global/selector';
-import { ACTIVE, LARGE } from '../../../scripts/global/const';
+import { ACTIVE } from '../../../scripts/global/const';
 import onEsc from '../../../scripts/global/onEsc';
 
 const modals = $$('.modal');
 const btnsClose = $$('.modal__close');
 const overlays = $$('.overlay--modal');
-const priceOrder = $('.modal--price-order');
-
-const onPriceOrder = () => {
-  if (window.innerWidth >= LARGE) {
-    priceOrder.classList.remove(ACTIVE);
-	}
-}
+const btnsOpenAppointment = $$('.js-appointment');
+const modalAppointment = $('.modal--appointment');
 
 const close = () => {
-  e.preventDefault();
   for (let i = 0; i < modals.length; i += 1) {
     modals[i].classList.remove(ACTIVE);
   }
 }
 
-const onEscKeyup = e => {
-  e.preventDefault();
-  onEsc(e, close());
-};
-
+// Закрывает модальное окно по клику на крестик
 for (let i = 0; i < btnsClose.length; i += 1) {
   btnsClose[i].addEventListener('click', () => {
-    btnsClose[i].parentNode.parentNode.classList.remove(ACTIVE);
+    btnsClose[i].parentNode.parentNode.parentNode.classList.remove(ACTIVE);
   });
 }
 
-// for (let i = 0; i < overlays.length; i += 1) {
-//   overlays[i].addEventListener('click', () => {
-//     overlays[i].parentNode.classList.remove(ACTIVE);
-//   });
-// }
-//
-// window.addEventListener('resize', onPriceOrder);
+// Закрывает модальное окно по клику на оверлей
+for (let i = 0; i < overlays.length; i += 1) {
+  overlays[i].addEventListener('click', () => {
+    overlays[i].parentNode.classList.remove(ACTIVE);
+  });
+}
+
+// включает модальное окно "Записаться на прием"
+for (let i = 0; i < btnsOpenAppointment.length; i += 1) {
+  btnsOpenAppointment[i].addEventListener('click', () => {
+    modalAppointment.classList.add(ACTIVE);
+  });
+}
+
+// Закрывает модальное окно при нажатии экскейп
+const onEscKeyup = e => {
+  e.preventDefault();
+  onEsc(e, close(e));
+};
