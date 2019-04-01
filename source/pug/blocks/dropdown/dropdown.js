afceade4a67fad646f2b97bb8b0e1824f8f8f8ce
loremlipsum <lipsum.webdevelop@gmail.com>
2019-04-01 14:53:25 +0300

    fix

--- source/pug/blocks/dropdown/dropdown.js
+++ source/pug/blocks/dropdown/dropdown.js
@@ -1,14 +1,11 @@
-import { $, $$ } from '../../../scripts/global/selector';
+import { $$ } from '../../../scripts/global/selector';
 import { ACTIVE } from '../../../scripts/global/const';
 
-const b = document.querySelectorAll('.dropdown__btn');
+const btns = $$('.dropdown__btn');
 
-
-for (let i = 0; i < b.length; i += 1) {
-  b[i].addEventListener('click', (e) => {
+for (let i = 0; i < btns.length; i += 1) {
+  btns[i].addEventListener('click', (e) => {
     e.preventDefault();
     e.currentTarget.parentNode.classList.toggle(ACTIVE);
   });
 }
-
-console.log('dsfsfdsfdsdf')
--- source/pug/blocks/modal/modal.js
+++ source/pug/blocks/modal/modal.js
@@ -2,52 +2,49 @@ import { $, $$ } from '../../../scripts/global/selector';
 import { ACTIVE } from '../../../scripts/global/const';
 import onEsc from '../../../scripts/global/onEsc';
 
-const modals = $$('.modal');
 const btnsClose = $$('.modal__close');
 const overlays = $$('.overlay--modal');
-const btnsOpenAppointment = $$('.js-appointment');
+const btnsModal = $$('.js-appointment');
+const modal = $('.modal--appointment');
 const btnVideo = $('.intro__video-btn');
-const modalAppointment = $('.modal--appointment');
 const video = $('.modal--intro-video');
 
-const close = () => {
-  for (let i = 0; i < modals.length; i += 1) {
-    modals[i].classList.remove(ACTIVE);
-  }
-}
 
 // Закрывает модальное окно по клику на крестик
 for (let i = 0; i < btnsClose.length; i += 1) {
-  btnsClose[i].addEventListener('click', () => {
-    btnsClose[i].parentNode.parentNode.parentNode.classList.remove(ACTIVE);
+  btnsClose[i].addEventListener('click', (e) => {
+    e.preventDefault();
+    e.currentTarget.parentNode.parentNode.parentNode.classList.remove(ACTIVE);
   });
 }
 
 // Закрывает модальное окно по клику на оверлей
 for (let i = 0; i < overlays.length; i += 1) {
-  overlays[i].addEventListener('click', () => {
-    overlays[i].parentNode.classList.remove(ACTIVE);
+  overlays[i].addEventListener('click', (e) => {
+    e.preventDefault();
+    e.currentTarget.parentNode.classList.remove(ACTIVE);
   });
 }
 
 // включает модальное окно "Записаться на прием"
-for (let i = 0; i < btnsOpenAppointment.length; i += 1) {
-  btnsOpenAppointment[i].addEventListener('click', () => {
-    modalAppointment.classList.add(ACTIVE);
+for (let i = 0; i < btnsModal.length; i += 1) {
+  btnsModal[i].addEventListener('click', (e) => {
+    e.preventDefault();
+    modal.classList.add(ACTIVE);
   });
 }
 
-const openVideo = () => {
-  video.classList.add(ACTIVE);
-}
-
-const onBtnVideoClick = (e) => {
-  e.preventDefault();
-  openVideo();
-}
-
 // включает модальное окно видео
-// btnVideo.addEventListener('click', onBtnVideoClick);
+if(btnVideo){
+  btnVideo.addEventListener('click', (e) => {
+    e.preventDefault();
+    video.classList.add(ACTIVE);
+  }, false);
+}
+
+video.querySelector('.modal__close').addEventListener('click', () => {
+  video.querySelector('.modal__video').contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+})
 
 // Закрывает модальное окно при нажатии экскейп
 const onEscKeyup = e => {
